6658e718cc04c95d0f2393bc386c184bd9de3794
qyvlik <qyvlik@qq.com>
2019-10-14 22:00:40 +0800

    add case

--- src/main/java/io/github/qyvlik/springhmacrestverify/modules/gateway/EchoController.java
+++ src/main/java/io/github/qyvlik/springhmacrestverify/modules/gateway/EchoController.java
@@ -1,7 +1,9 @@
 package io.github.qyvlik.springhmacrestverify.modules.gateway;
 
 import io.github.qyvlik.springhmacrestverify.common.base.ResponseObject;
+import io.github.qyvlik.springhmacrestverify.modules.gateway.request.PostRequest;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,16 @@ public class EchoController {
     }
 
     @RequestMapping(
+            method = RequestMethod.POST,
+            value = "api/v1/post-form-2",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseObject<String> echoPostForm2(PostRequest postRequest, String param3) {
+        return new ResponseObject<>("param1: " + postRequest.getParam1()
+                + ", param2: " + postRequest.getParam2() + ", param3: " + param3);
+    }
+
+    @RequestMapping(
             method = RequestMethod.PUT,
             value = "api/v1/put-form",
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
@@ -36,5 +48,35 @@ public class EchoController {
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseObject<String> echoDeleteForm(HttpServletRequest request, String param1, String param2) {
         return new ResponseObject<>("param1: " + param1 + ", param2: " + param2);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            value = "api/v1/post-json",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseObject<String> echoPostJSON(@RequestBody PostRequest postRequest, String param3) {
+        return new ResponseObject<>("param1: " + postRequest.getParam1()
+                + ", param2: " + postRequest.getParam2() + ", param3: " + param3);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            value = "api/v1/delete-json",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseObject<String> echoDeleteJSON(@RequestBody PostRequest postRequest, String param3) {
+        return new ResponseObject<>("param1: " + postRequest.getParam1()
+                + ", param2: " + postRequest.getParam2() + ", param3: " + param3);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = "api/v1/put-json",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseObject<String> echoPutJSON(@RequestBody PostRequest postRequest, String param3) {
+        return new ResponseObject<>("param1: " + postRequest.getParam1()
+                + ", param2: " + postRequest.getParam2() + ", param3: " + param3);
     }
 }
---
+++ src/main/java/io/github/qyvlik/springhmacrestverify/modules/gateway/request/PostRequest.java
@@ -0,0 +1,22 @@
+package io.github.qyvlik.springhmacrestverify.modules.gateway.request;
+
+public class PostRequest {
+    private String param1;
+    private String param2;
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String param1) {
+        this.param1 = param1;
+    }
+
+    public String getParam2() {
+        return param2;
+    }
+
+    public void setParam2(String param2) {
+        this.param2 = param2;
+    }
+}
